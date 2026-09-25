a01d074ccc75efcdfce3d3257ff61e3aef831ae6
andstyle <andermuchael@gmail.com>
2026-09-25 12:18:52 -0300

    feat: exibir retornos da costura no acompanhamento publico

--- supabase/workneo_shares.sql
+++ supabase/workneo_shares.sql
@@ -105,7 +105,9 @@ begin
     'id',r->>'id','listId',r->>'listId','listName',r->>'listName',
     'date',r->>'date','costureira',r->>'costureira','createdBy',coalesce(r->'createdBy','{}'::jsonb),
     'items',(select coalesce(jsonb_agg(jsonb_build_object(
-      'product',i->>'product','qty',i->>'qty','color',i->>'color','pedido',i->>'pedido','createdBy',coalesce(i->'createdBy','{}'::jsonb)
+      'product',i->>'product','qty',i->>'qty','returnedQty',coalesce(i->>'returnedQty','0'),'color',i->>'color','pedido',i->>'pedido',
+      'cliente',coalesce(i->>'cliente',''),'tracking',coalesce(i->>'tracking',i->>'codigoAcompanhamento',''),
+      'createdBy',coalesce(i->'createdBy','{}'::jsonb)
     )),'[]'::jsonb) from jsonb_array_elements(coalesce(r->'items','[]'::jsonb)) i)
   )),'[]'::jsonb)
   into v_reports
